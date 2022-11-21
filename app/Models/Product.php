7271d606cbec270b4dcd82eace36a55154551c65
luuduong99 <73738252+luuduong99@users.noreply.github.com>
2022-11-21 17:02:22 +0700

    feat: product - crud

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,41 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'name',
+        'image',
+        'price',
+        'color',
+        'description',
+        'maker_id',
+        'created_user',
+        'updated_user',
+        'deleted_user'
+    ];
+
+    // public function category()
+    // {
+    //     return $this->belongsToMany(Category::class, 'category_id', 'id');
+    // }
+
+    // protected $casts = [
+    //     'image' => 'array'
+    // ];
+
+    // protected function image(): Attribute
+    // {
+    //     return Attribute::make(
+    //         get: fn ($value) => json_decode($value, true),
+    //         // set: fn ($value) => json_encode($value),
+    //     );
+    // }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layout.master');
 })->name('home');
+
+Route::prefix('product')->group(function () {
+    //Route::get('products', [ProductController::class, 'index'])->name('product.home');
+    Route::get('create', [ProductController::class, 'create'])->name('product.create');
+    Route::post('store', [ProductController::class, 'store'])->name('product.store');
+    Route::get('show/{id}', [ProductController::class, 'show'])->name('product.show');
+    Route::put('edit/{id}', [ProductController::class, 'edit'])->name('product.edit');
+    Route::delete('destroy/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
+    Route::get('detail/{id}', [ProductController::class, 'detail'])->name('product.detail');
+});
